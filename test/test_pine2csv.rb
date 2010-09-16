402170abfb226f55a0c531af31315af16d1d0ad4
Ben Walton <bwalton@artsci.utoronto.ca>
2010-09-15 21:54:23 -0400

    add a few basic tests
    Signed-off-by: Ben Walton <bwalton@artsci.utoronto.ca>

--- test/test_pine2csv.rb
+++ test/test_pine2csv.rb
@@ -1,7 +1,24 @@
 require 'helper'
 
 class TestPine2csv < Test::Unit::TestCase
-  should "probably rename this file and start testing for real" do
-    flunk "hey buddy, you should probably rename this file and start testing for real"
+  context "Accept valid single lines" do
+    setup do
+      @p = Pine2CSV.new
+    end
+
+    should "take simple line" do
+      @p.abook = "ben\tben walton\tbwalton@example.org\n"
+      assert_equal @p.to_csv, "ben,ben walton,person,<bwalton@example.org>\n"
+    end
+
+    should "allow blank nickname" do
+      @p.abook = "\tben walton\tbwalton@example.org\n"
+      assert_equal @p.to_csv, "\"\",ben walton,person,<bwalton@example.org>\n"
+    end
+
+    should "allow blank name" do
+      @p.abook = "ben\t\tbwalton@example.org\n"
+      assert_equal @p.to_csv, "ben,\"\",person,<bwalton@example.org>\n"
+    end
   end
 end
