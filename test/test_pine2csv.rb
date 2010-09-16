2c10f8c00db189b10f5e5ceef172ca76f11c04e9
Ben Walton <bwalton@artsci.utoronto.ca>
2010-09-15 22:34:22 -0400

    Add 2 new basic tests
    Check to ensure we error if the abook data isn't a string.
    Check to ensure we require a value in the recipient field.
    
    Signed-off-by: Ben Walton <bwalton@artsci.utoronto.ca>

--- test/test_pine2csv.rb
+++ test/test_pine2csv.rb
@@ -12,6 +12,12 @@ class TestPine2csv < Test::Unit::TestCase
       end
     end
 
+    should "require string data" do
+      assert_raise Pine2CSV::Error do
+        @p.abook = @p
+      end
+    end
+
     should "take simple line" do
       @p.abook = "ben\tben walton\tbwalton@example.org\n"
       assert_equal @p.to_csv, "ben,ben walton,person,<bwalton@example.org>\n"
@@ -26,5 +32,13 @@ class TestPine2csv < Test::Unit::TestCase
       @p.abook = "ben\t\tbwalton@example.org\n"
       assert_equal @p.to_csv, "ben,\"\",person,<bwalton@example.org>\n"
     end
+
+    should "not allow blank recipient" do
+      @p.abook = "ben\tben walton\t\n"
+      assert_raise Pine2CSV::Error do
+        @p.to_csv
+      end
+    end
+
   end
 end
