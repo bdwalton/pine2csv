bd178a11af93e200830f2a48f26f0e0c44814a1c
Ben Walton <bwalton@artsci.utoronto.ca>
2010-09-15 21:45:56 -0400

    Add a Pine2CSV::Error class for use in exception passing
    Add a nested class Error, inheriting from Exception.  Raise a new
    exception of this class if abook is nil when Pine2CSV is passed.
    Ensure that abook is initialized to nil if no value is passed during
    Pine2CSV object instantiation.

--- lib/pine2csv.rb
+++ lib/pine2csv.rb
@@ -4,14 +4,22 @@ require 'polyglot'
 require 'pine_addressbook'
 
 class Pine2CSV
+
+  class Error < Exception
+    def initialize(msg)
+      super(msg)
+    end
+  end
+
   attr :abook, true
 
   def initialize(abook = nil)
-    @abook = abook if abook
+    @abook = abook
     @parser = PineAddressbookParser.new
   end
 
   def to_csv
+    raise Pine2CSV::Error.new("No address book data supplied.") unless abook
     tree = @parser.parse(@abook)
     if tree.nil?
       $stderr.puts @parser.failure_reason
